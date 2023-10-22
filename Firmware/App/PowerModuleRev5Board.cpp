4e4529acd4890aaa1bb932744a154d8a4b7037bc
Bryan Bendall <b_bendall@hotmail.com>
2023-10-21 22:58:09 -0400

    Reorganize and clean up board file

--- Firmware/App/PowerModuleRev5Board.cpp
+++ Firmware/App/PowerModuleRev5Board.cpp
@@ -1,10 +1,13 @@
 #include "BrytecBoard.h"
 
 #include "BoardHardware.h"
+#include "BoardPinInput.h"
+#include "BoardPinInputOutput.h"
 #include "CanBus.h"
 #include "Fram.h"
 #include "FramDeserializer.h"
 #include "L9966.h"
+#include "L9966Input.h"
 #include "PowerModuleRev5Defs.h"
 #include "PowerOutput.h"
 #include "PwmDriverOutput.h"
@@ -27,18 +30,23 @@
 namespace Brytec {
 
 // Single outputs
-using output1 = PowerOutput<PwmDriverOutput<5>, PwmDriverOutput<4>>;
-using output2 = PowerOutput<PwmDriverOutput<2>, Stm32Output<GPIOB_BASE, Den2_Pin>>;
-using output3 = PowerOutput<PwmDriverOutput<9>, PwmDriverOutput<8>>;
-using output4 = PowerOutput<PwmDriverOutput<11>, PwmDriverOutput<10>>;
-using output5 = PowerOutput<PwmDriverOutput<3>, Stm32Output<GPIOA_BASE, Den5_Pin>>;
-using output6 = PowerOutput<PwmDriverOutput<7>, PwmDriverOutput<6>>;
+using out1 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<5>, PwmDriverOutput<4>>, L9966Input<11>>;
+using out2 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<2>, Stm32Output<GPIOB_BASE, Den2_Pin>>, L9966Input<10>>;
+using out3 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<9>, PwmDriverOutput<8>>, L9966Input<13>>;
+using out4 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<11>, PwmDriverOutput<10>>, L9966Input<3>>;
+using out5 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<3>, Stm32Output<GPIOA_BASE, Den5_Pin>>, L9966Input<12>>;
+using out6 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<7>, PwmDriverOutput<6>>, L9966Input<4>>;
 
 // Dual outputs
-using output7 = PowerOutput<PwmDriverOutput<1>, Stm32Output<GPIOC_BASE, Den7_8_Pin>>;
-using output8 = PowerOutput<PwmDriverOutput<0>, Stm32Output<GPIOC_BASE, Den7_8_Pin>>;
-using output9 = PowerOutput<PwmDriverOutput<15>, PwmDriverOutput<14>>;
-using output10 = PowerOutput<PwmDriverOutput<12>, PwmDriverOutput<14>>;
+using out7 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<1>, Stm32Output<GPIOC_BASE, Den7_8_Pin>>, L9966Input<9>>;
+using out8 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<0>, Stm32Output<GPIOC_BASE, Den7_8_Pin>>, L9966Input<8>>;
+using out9 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<15>, PwmDriverOutput<14>>, L9966Input<1>>;
+using out10 = BoardPinInputOutput<PowerOutput<PwmDriverOutput<12>, PwmDriverOutput<14>>, L9966Input<2>>;
+
+// Inputs
+using in1 = BoardPinInput<L9966Input<5>>;
+using in2 = BoardPinInput<L9966Input<6>>;
+using in3 = BoardPinInput<L9966Input<7>>;
 
 FramDeserializer deserializer;
 
@@ -94,104 +102,115 @@ void BrytecBoard::setupBrytecCan(uint32_t mask, uint32_t filter)
 
 void BrytecBoard::setupPin(uint16_t index, IOTypes::Types type)
 {
-    if (type == IOTypes::Types::Output_Batt) {
-        switch (index) {
-        case BT_PIN_Pin_1_and_8:
-            output3::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_2_and_9:
-            output6::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_6_and_12:
-            output1::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_7_and_13:
-            output5::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_14_and_15:
-            output4::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_18_and_19:
-            output2::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_20:
-            output10::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_21:
-            output9::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_25:
-            output8::setDiagnostics(true);
-            break;
-        case BT_PIN_Pin_26:
-            output7::setDiagnostics(true);
-            break;
+    BoardHardware::setSpiL9966();
 
-        default:
-            break;
-        }
+    switch (index) {
+
+    // Inputs and Outputs
+    case BT_PIN_Pin_1_and_8:
+        out3::setupType(type);
+        break;
+    case BT_PIN_Pin_2_and_9:
+        out6::setupType(type);
+        break;
+    case BT_PIN_Pin_6_and_12:
+        out1::setupType(type);
+        break;
+    case BT_PIN_Pin_7_and_13:
+        out5::setupType(type);
+        break;
+    case BT_PIN_Pin_14_and_15:
+        out4::setupType(type);
+        break;
+    case BT_PIN_Pin_18_and_19:
+        out2::setupType(type);
+        break;
+    case BT_PIN_Pin_20:
+        out10::setupType(type);
+        break;
+    case BT_PIN_Pin_21:
+        out9::setupType(type);
+        break;
+    case BT_PIN_Pin_25:
+        out8::setupType(type);
+        break;
+    case BT_PIN_Pin_26:
+        out7::setupType(type);
+        break;
+
+    // Inputs only
+    case BT_PIN_Pin_22:
+        in1::setupType(type);
+        break;
+    case BT_PIN_Pin_23:
+        in2::setupType(type);
+        break;
+    case BT_PIN_Pin_24:
+        in3::setupType(type);
+        break;
+
+    default:
+        break;
     }
 }
 
 void BrytecBoard::shutdownAllPins()
 {
     HAL_GPIO_WritePin(User_Led_GPIO_Port, User_Led_Pin, GPIO_PIN_RESET);
-    output1::setValue(0.0f);
-    output2::setValue(0.0f);
-    output3::setValue(0.0f);
-    output4::setValue(0.0f);
-    output5::setValue(0.0f);
-    output6::setValue(0.0f);
-    output7::setValue(0.0f);
-    output8::setValue(0.0f);
-    output9::setValue(0.0f);
-    output10::setValue(0.0f);
-    output1::setDiagnostics(false);
-    output2::setDiagnostics(false);
-    output3::setDiagnostics(false);
-    output4::setDiagnostics(false);
-    output5::setDiagnostics(false);
-    output6::setDiagnostics(false);
-    output7::setDiagnostics(false);
-    output8::setDiagnostics(false);
-    output9::setDiagnostics(false);
-    output10::setDiagnostics(false);
+    out1::shutdown();
+    out2::shutdown();
+    out3::shutdown();
+    out4::shutdown();
+    out5::shutdown();
+    out6::shutdown();
+    out7::shutdown();
+    out8::shutdown();
+    out9::shutdown();
+    out10::shutdown();
+
+    BoardHardware::setSpiL9966();
+    L9966::softwareReset();
+    L9966::init();
 }
 
 float BrytecBoard::getPinValue(uint16_t index, IOTypes::Types type)
 {
     BoardHardware::setSpiL9966();
 
-    if (type == IOTypes::Types::Input_20V_Variable || type == IOTypes::Types::Input_5V_Variable) {
+    switch (index) {
 
-        L9966::VoltageRange range = L9966::VoltageRange::TwentyVolt;
-        if (type == IOTypes::Types::Input_5V_Variable)
-            range = L9966::VoltageRange::FiveVolt;
+    // Inputs and Outputs
+    case BT_PIN_Pin_1_and_8:
+        return out3::getValue(type);
+    case BT_PIN_Pin_2_and_9:
+        return out6::getValue(type);
+    case BT_PIN_Pin_6_and_12:
+        return out1::getValue(type);
+    case BT_PIN_Pin_7_and_13:
+        return out5::getValue(type);
+    case BT_PIN_Pin_14_and_15:
+        return out4::getValue(type);
+    case BT_PIN_Pin_18_and_19:
+        return out2::getValue(type);
+    case BT_PIN_Pin_20:
+        return out10::getValue(type);
+    case BT_PIN_Pin_21:
+        return out9::getValue(type);
+    case BT_PIN_Pin_25:
+        return out8::getValue(type);
+    case BT_PIN_Pin_26:
+        return out7::getValue(type);
 
-        switch (index) {
-            // case BT_PIN_Pin_22: // Input 1
-            //     return L9966::readIoVoltage(5, range);
-            // case BT_PIN_Pin_23: // Input 2
-            //     return L9966::readIoVoltage(6, range);
-            // case BT_PIN_Pin_24: // Input 3
-            //     return L9966::readIoVoltage(7, range);
+        // Inputs
+    case BT_PIN_Pin_22:
+        return in1::getValue(type);
+    case BT_PIN_Pin_23:
+        return in2::getValue(type);
+    case BT_PIN_Pin_24:
+        return in3::getValue(type);
 
-        case BT_PIN_Pin_22: // Input 1
-            return L9966::readIoResistance(5, L9966::ResistanceRange::RR2);
-        case BT_PIN_Pin_23: // Input 2
-            return L9966::readIoResistance(6, L9966::ResistanceRange::RR2);
-        case BT_PIN_Pin_24: // Input 3
-        {
-            // return L9966::readIoResistance(7, L9966::ResistanceRange::RR2);
-            float a = L9966::readIoResistance(7, L9966::ResistanceRange::RR1);
-            float b = L9966::readIoResistance(7, L9966::ResistanceRange::RR2);
-            // float c = L9966::readIoResistance(7, L9966::ResistanceRange::RR3);
-            return b;
-        }
-
-        default:
-            break;
-        }
+    default:
+        break;
     }
 
     return 0.0f;
@@ -245,33 +264,26 @@ float BrytecBoard::getPinVoltage(uint16_t index)
     BoardHardware::setSpiL9966();
 
     switch (index) {
-    case BT_PIN_Pin_1_and_8: // Output 3
-        return L9966::readIoVoltage(13, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_2_and_9: // Output 6
-        return L9966::readIoVoltage(4, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_6_and_12: // Output 1
-        return L9966::readIoVoltage(11, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_7_and_13: // Output 5
-        return L9966::readIoVoltage(12, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_14_and_15: // Output 4
-            return L9966::readIoVoltage(3, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_18_and_19: // Output 2
-            return L9966::readIoVoltage(10, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_20: // Output 10
-            return L9966::readIoVoltage(2, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_21: // Output 9
-            return L9966::readIoVoltage(1, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_25: // Output 8
-            return L9966::readIoVoltage(8, L9966::VoltageRange::TwentyVolt);
-    case BT_PIN_Pin_26: // Output 7
-            return L9966::readIoVoltage(9, L9966::VoltageRange::TwentyVolt);
-
-            // case BT_PIN_Pin_22: // Input 1
-            //     return L9966::readIoVoltage(5, L9966::VoltageRange::TwentyVolt);
-            // case BT_PIN_Pin_23: // Input 2
-            //     return L9966::readIoVoltage(6, L9966::VoltageRange::TwentyVolt);
-            // case BT_PIN_Pin_24: // Input 3
-            //     return L9966::readIoVoltage(7, L9966::VoltageRange::TwentyVolt);
+    case BT_PIN_Pin_1_and_8:
+        return out3::readVoltage();
+    case BT_PIN_Pin_2_and_9:
+        return out6::readVoltage();
+    case BT_PIN_Pin_6_and_12:
+        return out1::readVoltage();
+    case BT_PIN_Pin_7_and_13:
+        return out5::readVoltage();
+    case BT_PIN_Pin_14_and_15:
+        return out4::readVoltage();
+    case BT_PIN_Pin_18_and_19:
+        return out2::readVoltage();
+    case BT_PIN_Pin_20:
+        return out10::readVoltage();
+    case BT_PIN_Pin_21:
+        return out9::readVoltage();
+    case BT_PIN_Pin_25:
+        return out8::readVoltage();
+    case BT_PIN_Pin_26:
+        return out7::readVoltage();
 
     default:
         break;
@@ -301,34 +313,34 @@ void BrytecBoard::setPinValue(uint16_t index, IOTypes::Types type, float value)
     if (type == IOTypes::Types::Output_Batt) {
         switch (index) {
         case BT_PIN_Pin_1_and_8:
-            output3::setValue(value);
+            out3::setValue(value);
             break;
         case BT_PIN_Pin_2_and_9:
-            output6::setValue(value);
+            out6::setValue(value);
             break;
         case BT_PIN_Pin_6_and_12:
-            output1::setValue(value);
+            out1::setValue(value);
             break;
         case BT_PIN_Pin_7_and_13:
-            output5::setValue(value);
+            out5::setValue(value);
             break;
         case BT_PIN_Pin_14_and_15:
-            output4::setValue(value);
+            out4::setValue(value);
             break;
         case BT_PIN_Pin_18_and_19:
-            output2::setValue(value);
+            out2::setValue(value);
             break;
         case BT_PIN_Pin_20:
-            output10::setValue(value);
+            out10::setValue(value);
             break;
         case BT_PIN_Pin_21:
-            output9::setValue(value);
+            out9::setValue(value);
             break;
         case BT_PIN_Pin_25:
-            output8::setValue(value);
+            out8::setValue(value);
             break;
         case BT_PIN_Pin_26:
-            output7::setValue(value);
+            out7::setValue(value);
             break;
 
         default:
